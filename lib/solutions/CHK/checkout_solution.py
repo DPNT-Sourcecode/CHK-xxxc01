b178e7030598e9b62ba790d6622af3f806d12cb6
sbx_user1051 <sbx_user1051@169.254.22.21>
2022-07-08 21:01:13 +0000

    Fri Jul 08 21:01:13 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -5,6 +5,15 @@
 def checkout(skus: str):
     
     # First validate that we have A, B, C or D
-    allowed_skus = "ABCD"
-    if all(sku in allowed_skus for sku in skus) is not True:
+    allowed_skus = "ABCD"  # List of allowed SKUs
+    
+    sku_string_is_valid = True  # Assume valid 
+    
+    for sku in skus:  # Iterate SKUs
+        if sku not in allowed_skus:  # Check in allowed SKUs
+            sku_string_is_valid = False
+
+    if sku_string_is_valid == False:
         return -1
+
+
