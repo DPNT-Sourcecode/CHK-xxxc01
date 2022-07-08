b94f8e576459d68551e2a49860346f5028635b3d
sbx_user1051 <sbx_user1051@169.254.22.21>
2022-07-08 21:10:13 +0000

    Fri Jul 08 21:10:13 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -8,15 +8,29 @@ def checkout(skus: str):
     allowed_skus = "ABCD"  # List of allowed SKUs
     
     sku_string_is_valid = True  # Assume valid 
+
+    total = 0
+
+    # If SKUs is empty or not a string reject with -1
+    if skus == "" or isinstance(skus, str) is False:
+        return -1
     
     for sku in skus:  # Iterate SKUs
         if sku not in allowed_skus:  # Check in allowed SKUs
             sku_string_is_valid = False
+        # else:
+        #     if 
 
     if sku_string_is_valid == False:  # If contains foreign SKUs reject with -1
         return -1
 
+    else:
+        return True
+
     
+
+    
+
 
 
 
