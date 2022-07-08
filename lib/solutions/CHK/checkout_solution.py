38bbbde0560d88e348e79348c19ddb71ec4f8a1b
sbx_user1051 <sbx_user1051@169.254.22.21>
2022-07-08 20:58:13 +0000

    Fri Jul 08 20:58:13 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,5 +2,9 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
-def checkout(skus):
-    raise NotImplementedError()
+def checkout(skus: str):
+    
+    # First validate that we have A, B, C or D
+    allowed_skus = "ABCD"
+    if all(sku in allowed_skus for sku in skus) is not True:
+        return -1
--- test/solution_tests/CHK_R1/test_chk_r1.py
+++ test/solution_tests/CHK_R1/test_chk_r1.py
@@ -1,5 +1,5 @@
 from lib.solutions.CHK.checkout_solution import checkout
 
 class TestChk():
-    def test_checkout(self):
-        assert checkout("ABCD") == 115
+    def test_checkout_has_only_allowed_skus(self):
+        assert checkout("ABC") == -1
